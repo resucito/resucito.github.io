52682cbd97fb0efc35f541400fae25582650a8a3
galpzi <ajplata0@gmail.com>
2021-05-03 07:08:42 -0500

    :package: Add static adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -4,15 +4,10 @@ const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
 
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: node(),
+		adapter: require('@sveltejs/adapter-static')(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
